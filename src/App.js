3ea374c3ed884008e0eae1e7492bc3c26eb49383
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2023-03-25 20:04:55 +0900

    feat:add color picker

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import "./App.css";
 import { fabric } from "fabric";
-import React, { useState, useEffect, useRef } from "react";
+import { ChromePicker } from "react-color";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import initCanvas from "./utils/initCanvas";
 
 const App = () => {
   // 초기 캔버스 셋팅
   const canvas = useRef(null);
-  const [canvasSize, setCanvasSize] = useState({ width: 800, height: 800 });
+  const [canvasSize, setCanvasSize] = useState({ width: 500, height: 500 });
+  const [objName, setObjName] = useState([]);
+  const [fillColor, setFillColor] = useState("");
+  const [strokeColor, setStrokeColor] = useState("");
+  const [objectId, setObjectId] = useState(0);
+  const [selectedObj, setSelectedObj] = useState(null);
+  const [stroke, setStroke] = useState(0);
   useEffect(() => {
     canvas.current = initCanvas(canvasSize);
-
     canvas.current.on("mouse:over", () => {
       console.log("hello");
     });
+    // canvas.current.on("object:scaling", (e) => {
+    //   var obj = e.target;
+    //   obj.strokeWidth
+    //   obj.strokeWidth = obj.strokeWidth / ((obj.scaleX + obj.scaleY) / 2);
+    //   var activeObject = canvas.current.getActiveObject();
+    //   activeObject.set("strokeWidth", obj.strokeWidth);
+    //   canvas.current.renderAll();
+    // }); 개쓸모없는코드
 
-    // destroy fabric on unmount
     return () => {
       canvas.current.dispose();
       canvas.current = null;
@@ -25,12 +38,21 @@ const App = () => {
     let rect = new fabric.Rect({
       left: 100,
       top: 100,
-      fill: "red",
+      fill: "#039BE5",
       width: 100,
       height: 100,
       angle: 0,
+      kindOf: "rect" + objectId,
     });
-
+    rect.on("selected", (e) => {
+      console.log("제발", e.target);
+      console.log(e.target.fill, e.target.stroke, e.target.kindOf);
+      setSelectedObj(e.target.kindOf);
+      setFillColor(e.target.fill);
+      setStrokeColor(e.target.stroke ? e.target.stroke : "#000000");
+      setStroke(e.target.strokeWidth);
+    });
+    setObjectId(objectId + 1);
     canvas.current.add(rect);
     canvas.current.renderAll();
   };
@@ -39,7 +61,7 @@ const App = () => {
       radius: 65,
       fill: "#039BE5",
       left: 0,
-      stroke: "red",
+      stroke: "#123456",
       strokeWidth: 3,
     });
     canvas.current.add(circle);
@@ -69,6 +91,49 @@ const App = () => {
     canvas.current.renderAll();
   };
 
+  const handlefillColorChange = useCallback(
+    (color) => {
+      console.log("getActiveObject", canvas.current.getActiveObject());
+      if (canvas.current.getActiveObject()) {
+        const activeObj = canvas.current.getActiveObject();
+        activeObj.set("fill", color);
+        canvas.current.renderAll();
+        setFillColor(color);
+      }
+    },
+    [fillColor]
+  );
+  const handleStrokeColorChange = useCallback(
+    (color) => {
+      // const objects = canvas.current.getObjects();
+      // for (const object in objects) {
+      //   if (objects[object].kindOf === selectedObj) {
+      //     // objects[object].setFill(color)
+      //   }
+      // }
+      // console.log("objects", objects);
+      if (canvas.current.getActiveObject()) {
+        console.log("getActiveObject", canvas.current.getActiveObject());
+        const activeObj = canvas.current.getActiveObject();
+        activeObj.set("stroke", color);
+        canvas.current.renderAll();
+        setStrokeColor(color);
+      }
+    },
+    [strokeColor]
+  );
+  const handleStrokeWidth = useCallback(
+    (e) => {
+      const activeObj = canvas.current.getActiveObject();
+      console.log("??", e.target.value, activeObj.setStrokeWidth);
+      activeObj.set("strokeWidth", Number(e.target.value));
+      canvas.current.renderAll();
+      // activeObj.set("padding", 0);
+      setStroke(e.target.value);
+    },
+    [stroke]
+  );
+
   return (
     <div className="App">
       <canvas id="canvas" />
@@ -80,6 +145,27 @@ const App = () => {
       <button onClick={increaseWidth}>캔버스 가로 늘이기</button>
       <button onClick={reduceHeight}>캔버스 세로 줄이기</button>
       <button onClick={increaseHeight}>캔버스 세로 늘이기</button>
+      <div style={{ display: "flex" }}>
+        <ChromePicker
+          color={fillColor}
+          onChange={(color) => handlefillColorChange(color.hex)}
+        />
+        <div>
+          stroke 설정
+          <ChromePicker
+            color={strokeColor}
+            onChange={(color) => handleStrokeColorChange(color.hex)}
+          />
+          <p>굵기 : {stroke}px</p>
+          <input
+            type="range"
+            min="1"
+            max="50"
+            value={stroke}
+            onChange={handleStrokeWidth}
+          />
+        </div>
+      </div>
     </div>
   );
 };
